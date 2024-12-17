bd06eef9801c9f3cf036b35b39296bf1770e6754
Raffaele Bortolotto <rbortolotto@alpenite.com>
2024-12-17 18:21:25 +0100

    scripts added

---
+++ app/backend_scripts/product_view.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         Magento 2 > BE > PRODUCT > VIEW
+// @namespace    M2Scripts
+// @version      2024-10-28
+// @description  Get info from current product
+// @author       rbortolotto
+// @match        *://*/*catalog/product/index*/*
+// @supportURL   https://github.com/Theriol666/m2scripts
+// @updateURL    https://github.com/Theriol666/m2scripts/raw/main/app/backend_script/product_view.js
+// @updateURL    https://github.com/Theriol666/m2scripts/raw/main/app/backend_script/product_view.js
+// @require      https://github.com/Theriol666/m2scripts/raw/main/app/backend_app.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_setClipboard
+// @run-at       document-body
+// @noframes
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    function getProductInformation() {
+        let sku = jQuery('input[name="product[sku]"]').val();
+        if (typeof document.m2scripts !== undefined && sku) {
+            alert(JSON.stringify(document.M2Scripts.makeApiCall('/rest/all/V1/products/' + sku), null, 2));
+        } else {
+            alert("Require JS not loaded yet");
+        }
+    }
+
+    function addButtons() {
+        const container = document.querySelector('.m2scripts-contianer');
+        if (container === null) {
+            console.log("Error: no container for M2 Scripts");
+        }
+
+        document.M2Scripts.addButtonToMainContainer("Get Product Information", getProductInformation ,"getProductInformation");
+    }
+
+    function start(){
+        M2Scripts.isReady(function () {ù
+            addButtons();
+        });
+    }
+
+    start();
+})();
